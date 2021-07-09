8e24ff66558b73318c8b6ada141a684ea827703a
vishnu <vishnudevxamarin.gmail.com>
2021-07-09 20:46:22 +0530

    Stroage permissions

--- EssentialsDemo/MainPage.xaml.cs
+++ EssentialsDemo/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace EssentialsDemo
@@ -17,7 +18,57 @@ namespace EssentialsDemo
 
         private void CheckPermission(object sender, EventArgs e)
         {
-
+            CheckAndRequestStroageReadPermission();
+           // CheckAndRequestStroagWritePermission();
         }
+
+
+        public async Task<PermissionStatus> CheckAndRequestStroageReadPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.StorageRead>();
+
+            if (status == PermissionStatus.Granted)
+                return status;
+
+            if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                // Prompt the user to turn on in settings
+                // On iOS once a permission has been denied it may not be requested again from the application
+                return status;
+            }
+
+            if (Permissions.ShouldShowRationale<Permissions.StorageRead>())
+            {
+                // Prompt the user with additional information as to why the permission is needed
+               
+            }
+
+            status = await Permissions.RequestAsync<Permissions.StorageRead>();
+
+            return status;
+        }
+        //public async Task<PermissionStatus> CheckAndRequestStroagWritePermission()
+        //{
+        //    var status = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
+
+        //    if (status == PermissionStatus.Granted)
+        //        return status;
+
+        //    if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+        //    {
+        //        // Prompt the user to turn on in settings
+        //        // On iOS once a permission has been denied it may not be requested again from the application
+        //        return status;
+        //    }
+
+        //    if (Permissions.ShouldShowRationale<Permissions.StorageWrite>())
+        //    {
+        //        // Prompt the user with additional information as to why the permission is needed
+        //    }
+
+        //    status = await Permissions.RequestAsync<Permissions.StorageWrite>();
+
+        //    return status;
+        //}
     }
 }
